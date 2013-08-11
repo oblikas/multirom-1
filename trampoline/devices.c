a8f9a44b92bde5d8262cf3fedf2924c8d01287b1
oblikas <alex.stoian98@yahoo.com>
2013-08-11 23:27:29 +0300

    Porting to mako

--- trampoline/devices.c
+++ trampoline/devices.c
@@ -59,41 +59,38 @@ static struct selabel_handle *sehandle;
 
 static const char *uevent_paths[] =
 {
-    "/sys/devices/tegradc.0/graphics/fb0",
+    "/sys/class/graphics/fb0",
 
     "/sys/block/mmcblk0",
-    "/sys/devices/platform/sdhci-tegra.3/mmc_host",
-    "/sys/devices/platform/sdhci-tegra.3/mmc_host/mmc0",
-    "/sys/devices/platform/sdhci-tegra.3/mmc_host/mmc0/mmc0:0001",
-    "/sys/devices/platform/sdhci-tegra.3/mmc_host/mmc0/mmc0:0001/block/mmcblk0",
-    "/sys/devices/platform/sdhci-tegra.3/mmc_host/mmc0/mmc0:0001/block/mmcblk0/mmcblk0p9",
-    "/sys/devices/platform/sdhci-tegra.3/mmc_host/mmc0/mmc0:0001/block/mmcblk0/mmcblk0p10",
-    "/sys/devices/platform/sdhci-tegra.3/mmc_host/mmc0/mmc0:0001/block/mmcblk0/mmcblk0p2",
+    "/sys/devices/platform/msm_sdcc.1/mmc_host",
+    "/sys/devices/platform/msm_sdcc.1/mmc_host/mmc0",
+    "/sys/devices/platform/msm_sdcc.1/mmc_host/mmc0/mmc0:0001",
+    "/sys/devices/platform/msm_sdcc.1/mmc_host/mmc0/mmc0:0001/block/mmcblk0",
+    "/sys/devices/platform/msm_sdcc.1/mmc_host/mmc0/mmc0:0001/block/mmcblk0/mmcblk0p6",
+    "/sys/devices/platform/msm_sdcc.1/mmc_host/mmc0/mmc0:0001/block/mmcblk0/mmcblk0p23",
+    "/sys/devices/platform/msm_sdcc.1/mmc_host/mmc0/mmc0:0001/block/mmcblk0/mmcblk0p21",
     "/sys/bus/mmc",
     "/sys/bus/mmc/drivers/mmcblk",
-    "/sys/bus/mmc/drivers/mmc_test",
     "/sys/bus/sdio/drivers/bcmsdh_sdmmc",
     "/sys/module/mmc_core",
     "/sys/module/mmcblk",
 
-    "/sys/devices/platform/gpio-keys.0/input/input2",
-    "/sys/devices/platform/gpio-keys.0/input/input2/event2",
-    "/sys/devices/virtual/misc/uinput",
-    "/sys/devices/virtual/input/input0",
-    "/sys/devices/virtual/input/input0/event0",
-    "/sys/devices/virtual/input/input1",
-    "/sys/devices/virtual/input/input1/event1",
-
+    "/sys/class/input/input2",
+    "/sys/class/input/input2/event2",
+    "/sys/class/misc/uinput",
+    "/sys/class/input/input0",
+    "/sys/class/input/input0/event0",
+    "/sys/class/input/input1",
+    "/sys/class/input/input1/event1",
+    
     // for adb
-    "/sys/devices/virtual/tty/ptmx",
-    "/sys/devices/platform/sdhci-tegra.3/mmc_host/mmc0/mmc0:0001/block/mmcblk0/mmcblk0p3", // /system
-    "/sys/devices/platform/sdhci-tegra.3/mmc_host/mmc0/mmc0:0001/block/mmcblk0/mmcblk0p4", // /cache
-    "/sys/devices/virtual/misc/android_adb",
-    "/sys/devices/virtual/android_usb/android0/f_adb",
+    "/sys/class/tty/ptmx",
+    "/sys/devices/platform/msm_sdcc.1/mmc_host/mmc0/mmc0:0001/block/mmcblk0/mmcblk0p21", //system
+    "/sys/devices/platform/msm_sdcc.1/mmc_host/mmc0/mmc0:0001/block/mmcblk0/mmcblk0p22", // /cache
+    "/sys/class/misc/android_adb",
+    "/sys/class/android_usb/android0/f_adb",
     "/sys/bus/usb",
 
-    // USB drive is in here
-    "/sys/devices/platform/tegra-ehci.0*",
 
     NULL
 };
